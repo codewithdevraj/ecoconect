0e66ae50f5eae00c7057d1a3ad816d9ab13e02c3
Amrit Pandey <amritpandey9695@gmail.com>
2024-08-01 17:14:11 +0530

    updated

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
 import "./style.css"; // Ensure you have the relevant CSS for styling
 
 const Header = () => {
---
+++ src/components/pages/forms/login.jsx
@@ -0,0 +1,37 @@
+import "./index.css";
+
+const LoginForm = () => {
+    return(
+       <section className="formbody">
+         <div className="container">
+      <header>Login Form</header>
+      <form action="#" className="form">
+        <div className="input-box">
+          <label>Email Address</label>
+          <input type="email" placeholder="Enter email address" required />
+        </div>
+        <div className="input-box">
+          <label>Password</label>
+          <input type="password" placeholder="Enter email address" required />
+        </div>
+
+        <button>Login</button>
+        <div className="help-btn">
+          <div className="signupref">
+            <p>Don't have an account?</p>
+            <a href="">Create account</a>
+          </div>
+          <div className="forget">
+            <p>
+                Didn't remember your password?
+            </p>
+            <a href="/forget">Forget Password?</a>
+          </div>
+        </div>
+      </form>
+    </div>
+       </section>
+    )
+}
+
+export default LoginForm;
