9fe295dc57435d0a52a771b69eeb8a0d3d2dfef2
Devraj <dev.raj.697492@gmail.com>
2024-08-02 15:08:46 +0530

    updates

---
+++ src/components/pages/blogs/layouts/hero.jsx
@@ -0,0 +1,16 @@
+
+const BlogHero = () => {
+  return (
+    <section className="community-hero">
+      <div className="text-wrap">
+        <h1>Welcome to Our Blog</h1>
+        <p>
+          Your source for the latest news, articles, and information on
+          Sustainable Development Goals (SDGs).
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default BlogHero;
